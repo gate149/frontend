b69e6e65d75baa9856d427a89672f8b0042cb180
Alex <pav.perm.17@yandex.ru>
2025-11-19 19:51:00 +0300

    feat: added footer with privacy policy

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,5 +1,25 @@
+import { Container, Group, Text, Anchor } from '@mantine/core';
+import Link from 'next/link';
+import classes from './styles.module.css';
+
 const Footer = () => {
-    return <></>
+    const currentYear = new Date().getFullYear();
+
+    return (
+        <div className={classes.footer}>
+            <Container className={classes.container} size="xl">
+                <Text c="dimmed" size="sm">
+                    © {currentYear} gate149 inc. Все права защищены.
+                </Text>
+
+                <Group className={classes.links}>
+                    <Anchor component={Link} href="/privacy" c="dimmed" size="sm">
+                        Политика конфиденциальности
+                    </Anchor>
+                </Group>
+            </Container>
+        </div>
+    );
 }
 
-export {Footer};
+export { Footer };
